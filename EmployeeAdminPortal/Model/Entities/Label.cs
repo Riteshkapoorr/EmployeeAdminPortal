886f383186a6d1ce864d6ae84c95432d3debcd91
Ritesh <riteshkapoor652@gmail.com>
2024-12-12 19:11:23 +0500

    Add project files.

---
+++ EmployeeAdminPortal/Model/Entities/Label.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeAdminPortal.Model.Entities
+{
+
+    public class Label
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+
+}
---
+++ EmployeeAdminPortal/Model/Entities/Task.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeAdminPortal.Model.Entities
+{
+    public class Task
+    {
+        public required Label Label { get; set; }
+        public required string Name { get; set; }
+
+        public string? Desc { get; set; }
+        public required DateTime DateAdded { get; set; }
+    }
+}
